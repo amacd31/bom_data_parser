9e75691b565734f6ad723ba48dace5f56d4d58f5
Andrew MacDonald <andrew@maccas.net>
2014-09-04 20:58:39 +1000

    Basic ACORN-SAT csv reader.

--- bom_data_parser/__init__.py
+++ bom_data_parser/__init__.py
@@ -1,1 +1,2 @@
 from climate_data_online import read_climate_data_online_csv
+from acorn_sat import read_acorn_sat_csv
---
+++ bom_data_parser/acorn_sat.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def read_acorn_sat_csv(fname):
+    with open(fname) as f:
+        header_line = f.readline()
+
+    measurand = header_line[0:8]
+
+    missing_value_key = 'missing_value='
+    missing_value_start = header_line.index(missing_value_key)+len(missing_value_key)
+    missing_value = header_line[missing_value_start:].split(' ')[0]
+
+    df = pd.read_csv(fname, parse_dates=[0], index_col=0, sep=r"\s+", header=None, skiprows=1, na_values=missing_value, names=['Date',measurand])
+
+    return df, None
