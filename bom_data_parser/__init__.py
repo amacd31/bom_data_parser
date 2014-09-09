a122332c251437737a6153e776ceed35afa05160
Andrew MacDonald <andrew@maccas.net>
2014-09-09 22:11:39 +1000

    Read method for the 72 hour observations JSON.

--- bom_data_parser/__init__.py
+++ bom_data_parser/__init__.py
@@ -1,2 +1,3 @@
 from climate_data_online import read_climate_data_online_csv
 from acorn_sat import read_acorn_sat_csv
+from observations_json import read_obs_json
---
+++ bom_data_parser/observations_json.py
@@ -0,0 +1,36 @@
+import json
+import pandas as pd
+
+def parse_json(station_json):
+    if station_json.keys() != [u'observations']:
+        raise Exception('No observations key; Not 72 hour observations json format')
+
+    attributes = {
+            'station_id': station_json['observations']['header'][0]['ID'],
+            'name': station_json['observations']['data'][0]['name'],
+            'history_product': station_json['observations']['data'][0]['history_product'],
+            'wmo': station_json['observations']['data'][0]['wmo'],
+            'lat': station_json['observations']['data'][0]['lat'],
+            'lon': station_json['observations']['data'][0]['lon'],
+            'header': station_json['observations']['header'][0],
+            'notice': station_json['observations']['notice'][0]
+            }
+
+    df = pd.read_json(json.dumps(station_json['observations']['data']))
+    df['aifstime_utc'] = pd.to_datetime(df.aifstime_utc.map(str), format="%Y%m%d%H%M%S", utc=True)
+
+    df['local_date_time_full'] = pd.to_datetime(df.local_date_time_full.map(str), format="%Y%m%d%H%M%S")
+
+    df = df.set_index('aifstime_utc')
+    df = df.drop('history_product',1)
+    df = df.drop('name',1)
+    df = df.drop('wmo',1)
+    df = df.drop('lat',1)
+    df = df.drop('lon',1)
+    df = df.drop('sort_order',1)
+    df = df.sort_index()
+
+    return df, attributes
+
+def read_obs_json(filename):
+    return parse_json(json.load(file(filename)))
