18cb78fafab207c7b73fd5b28232c589b8b36b57
Andrew MacDonald <andrew@maccas.net>
2014-09-13 15:19:46 +1000

    Use setup.py and versioneer to make installable.

--- bom_data_parser/__init__.py
+++ bom_data_parser/__init__.py
@@ -2,3 +2,7 @@ from bom_data_parser.acorn_sat import read_acorn_sat_csv
 from bom_data_parser.climate_data_online import read_climate_data_online_csv
 from bom_data_parser.hrs import read_hrs_csv
 from bom_data_parser.observations_json import read_obs_json
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ setup.py
@@ -0,0 +1,37 @@
+import versioneer
+versioneer.versionfile_source = 'bom_data_parser/_version.py'
+versioneer.versionfile_build = 'bom_data_parser/_version.py'
+versioneer.tag_prefix = 'v'
+versioneer.parentdir_prefix = 'bom_data_parser-'
+
+from setuptools import setup
+
+setup(
+    name='bom_data_parser',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Basic library for parsing data formats supplied by the Australian Bureau of Meteorology.',
+    author='Andrew MacDonald',
+    author_email='andrew@maccas.net',
+    license='BSD',
+    url='https://github.com/amacd31/bom_data_parser',
+    install_requires=['numpy', 'pandas'],
+    packages = ['bom_data_parser'],
+    test_suite = 'tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
