4aadc1d6a80897e99ba91b9bf0547b5ab5f23c8b
Andrew MacDonald <andrew@maccas.net>
2014-09-13 15:41:36 +1000

    Fix test to use int instead of hex (don't prefix 0).

--- tests/test_hrs.py
+++ tests/test_hrs.py
@@ -21,8 +21,8 @@ class HRSTest(unittest.TestCase):
         self.assertEqual(attributes['latitude'], 148.8212)
         self.assertEqual(attributes['longitude'], -35.5917)
 
-        self.assertEqual(data.index[0], datetime(1963,07,05))
-        self.assertEqual(data.index[-1], datetime(2012,10,04))
+        self.assertEqual(data.index[0], datetime(1963,7,5))
+        self.assertEqual(data.index[-1], datetime(2012,10,4))
         self.assertAlmostEqual(data.Q.values[0], 127.312,3)
         self.assertAlmostEqual(data.Q.values[-1], 186.238,3)
         self.assertEqual(data.QCode.values[0], 10)
