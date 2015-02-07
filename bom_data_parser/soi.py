dcbd202da1adefdbddaaef8a3147711ad1e7c20e
Andrew MacDonald <andrew@maccas.net>
2015-02-08 10:20:01 +1100

    StringIO import handling for Python3 compat.

--- bom_data_parser/soi.py
+++ bom_data_parser/soi.py
@@ -1,5 +1,8 @@
 import pandas as pd
-import StringIO
+try:
+    from StringIO import StringIO
+except:
+    from io import StringIO
 
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -7,7 +10,7 @@ from datetime import datetime
 def read_soi_html(input_file):
     soup = BeautifulSoup(input_file.read())
 
-    soi_data = pd.read_table(StringIO.StringIO(soup.pre.text.replace('*', '').strip()), index_col='Year').drop('Unnamed: 13', 1).unstack()
+    soi_data = pd.read_table(StringIO(soup.pre.text.replace('*', '').strip()), index_col='Year').drop('Unnamed: 13', 1).unstack()
 
     soi_data = pd.DataFrame({"soi": soi_data.values}, index=soi_data.index.map(lambda i: datetime.strptime("{0} {1}".format(i[0], i[1]), "%b %Y"))).sort_index().dropna().asfreq('MS')
 
