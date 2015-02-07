3be421b25fad1e02f77600345823705fbaa5f1f5
Andrew MacDonald <andrew@maccas.net>
2015-02-07 22:00:27 +1100

    Clean up test_soi.py.

--- tests/test_soi.py
+++ tests/test_soi.py
@@ -8,11 +8,7 @@ from bom_data_parser import read_soi_html
 
 class SOITest(unittest.TestCase):
     def setUp(self):
-        #self.test_soi_file = open(os.path.join(os.path.dirname(__file__), 'data', 'SOI', 'soiplaintext.html'), 'r')
         self.test_soi_file = os.path.join(os.path.dirname(__file__), 'data', 'SOI', 'soiplaintext.html')
-
-    #def tearDown(self):
-        #self.test_soi_file.close()
 
     def test_soi(self):
         with open(self.test_soi_file, 'r') as soi_file:
