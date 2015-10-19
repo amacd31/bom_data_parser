c5c459cc260b1cdd974dff57661391b641885d11
Andrew MacDonald <andrew@maccas.net>
2015-10-19 14:44:30 +1100

    Add method to read CDO data straight from zip file

--- bom_data_parser/__init__.py
+++ bom_data_parser/__init__.py
@@ -1,5 +1,6 @@
 from bom_data_parser.acorn_sat import read_acorn_sat_csv
 from bom_data_parser.climate_data_online import read_climate_data_online_csv
+from bom_data_parser.climate_data_online import read_climate_data_online_zip
 from bom_data_parser.hrs import read_hrs_csv
 from bom_data_parser.observations_json import read_obs_json
 from bom_data_parser.soi import read_soi_html
--- bom_data_parser/climate_data_online.py
+++ bom_data_parser/climate_data_online.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import zipfile
 
 from bom_data_parser import mapper
 
@@ -25,5 +26,19 @@ def read_climate_data_online_csv(fname):
         assert np.all(df[column] == df[column][0])
         attributes[column] = df[column][0]
         df = df.drop(column, axis = 1)
+
+    return df, attributes
+
+def read_climate_data_online_zip(fname):
+    """
+        Read data straight out of zipfile.
+
+        ..note: Reuqires the filename to have been unchanged because it is used for identifying the contained data file.
+    """
+    filename = fname.split('.')[0]
+    with zipfile.ZipFile('{0}.zip'.format(filename)) as zf:
+        #notes = zf.read('{0}_Note.txt'.format(filename))
+        with zf.open('{0}_Data.csv'.format(filename)) as dfile:
+            df, attributes = read_climate_data_online_csv(dfile)
 
     return df, attributes
