3075089f24425b2924938b1dc24349f3a9f06e7c
Andrew MacDonald <andrew@maccas.net>
2015-10-29 21:15:27 +1100

    Specify lxml parser for BeautifulSoup

--- bom_data_parser/soi.py
+++ bom_data_parser/soi.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 def read_soi_html(input_file):
-    soup = BeautifulSoup(input_file.read())
+    soup = BeautifulSoup(input_file.read(), "lxml")
 
     soi_data = pd.read_table(StringIO(soup.pre.text.replace('*', '').strip()), index_col='Year', sep='\s+').unstack()
 
