d1b10776d7a652f1adf9fa1c742cb85c2bcd61c4
Andrew MacDonald <andrew@maccas.net>
2015-10-29 21:48:32 +1100

    Rename file handle in water storage unit test

--- tests/test_water_storage.py
+++ tests/test_water_storage.py
@@ -9,8 +9,8 @@ class WaterStoragesTest(unittest.TestCase):
         self.test_xml_file = os.path.join(os.path.dirname(__file__), 'data', 'water_storage', 'urn:bom.gov.au:awris:common:codelist:feature:lakenillahcootie')
 
     def test_read_storage(self):
-        with open(self.test_xml_file, 'r') as soi_file:
-            storage_data = read_water_storage_series(soi_file)
+        with open(self.test_xml_file, 'r') as water_storage_file:
+            storage_data = read_water_storage_series(water_storage_file)
 
         self.assertEqual(storage_data.ix['2015-10-27'], 20929.055)
         self.assertAlmostEqual(storage_data.ix['2013-01-01'], 34895.71)
