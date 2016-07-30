df3cc8495269cf4f974ce44b1d5c663749d84311
Andrew MacDonald <andrew@maccas.net>
2016-07-30 15:51:27 +1000

    Update to handle October 2015 format HRS data

--- bom_data_parser/hrs.py
+++ bom_data_parser/hrs.py
@@ -1,19 +1,36 @@
+import logging
 import pandas as pd
 
 from bom_data_parser import mapper
 
+logger = logging.getLogger(__name__)
+
 def read_hrs_csv(fname):
     attributes = {}
-    hrs_header_len = 18
+    hrs_header_len = 26
     with open(fname) as f:
         header=[next(f) for x in range(hrs_header_len)]
-        attributes['station_name'] = header[10][3:-2]
-        attributes['catchment_area'] = float(header[11].split(',')[2])
-        location = header[12].split(',')
-        attributes['latitude'] = float(location[2])
-        attributes['longitude'] = float(location[4]) * -1 # Negative for 'degrees S'
-        header = ''.join(header)
+        logging.debug(header)
+        if header[3] == '#,"Daily streamflow (ML/day) and quality code"\n':
+            logging.debug("Original format HRS")
+            hrs_header_len = 18
+            attributes['station_name'] = header[10][3:-2]
+            attributes['catchment_area'] = float(header[11].split(',')[2])
+            location = header[12].split(',')
+            attributes['latitude'] = float(location[2])
+            attributes['longitude'] = float(location[4]) * -1 # Negative for 'degrees S'
+        elif header[3] == '#,"Dataset version: October, 2015"\n':
+            logging.debug("October 2015, format HRS")
+            hrs_header_len = 26
+            attributes['station_name'] = header[14][3:-2]
+            attributes['catchment_area'] = float(header[15].split(',')[2])
+            location = header[16].split(',')
+            attributes['latitude'] = float(location[2])
+            attributes['longitude'] = float(location[4]) * -1 # Negative for 'degrees S'
+        else:
+            raise NotImplementedError("Unsupported HRS data format.")
 
+    logging.debug("hrs_header_len: %s", hrs_header_len)
     df = pd.read_csv(fname, parse_dates=True, index_col='Date', skiprows=hrs_header_len)
 
     return df, attributes
--- tests/test_hrs.py
+++ tests/test_hrs.py
@@ -27,3 +27,22 @@ class HRSTest(unittest.TestCase):
         self.assertAlmostEqual(data.Q.values[-1], 186.238,3)
         self.assertEqual(data.QCode.values[0], 10)
         self.assertEqual(data.QCode.values[-1], 10)
+
+    def test_hrs_201510_format(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'data', 'HRS', '410730_daily_ts_201510.csv')
+        data, attributes = read_hrs_csv(test_file)
+
+        self.assertTrue('Flow (ML)' in data.columns)
+        self.assertTrue('Bureau QCode' in data.columns)
+
+        self.assertEqual(attributes['station_name'], 'Cotter River at Gingera (410730)')
+        self.assertEqual(attributes['catchment_area'], 130.0)
+        self.assertEqual(attributes['latitude'], 148.8212)
+        self.assertEqual(attributes['longitude'], -35.5917)
+
+        self.assertEqual(data.index[0], datetime(1963,7,5))
+        self.assertEqual(data.index[-1], datetime(2014,12,31))
+        self.assertAlmostEqual(data['Flow (ML)'].values[0], 127.322,3)
+        self.assertAlmostEqual(data['Flow (ML)'].values[-1], 16.1915,4)
+        self.assertEqual(data['Bureau QCode'].values[0], 'A')
+        self.assertEqual(data['Bureau QCode'].values[-1], 'A')
